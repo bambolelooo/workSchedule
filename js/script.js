c3ac751000b3ece943eff43127fc882825369ffc
bambolelooo <63548697+bambolelooo@users.noreply.github.com>
2022-11-06 16:53:23 -0500

    added popup on save

--- js/script.js
+++ js/script.js
@@ -45,5 +45,6 @@ $(function () {
         tasks[hourCount] = $(this).siblings("textarea").val();
         console.log(tasks);
         localStorage.setItem("tasks", JSON.stringify(tasks));
+        $(".popup").css("display", "flex").hide().fadeIn(1000).fadeOut(1000);
     });
 });
